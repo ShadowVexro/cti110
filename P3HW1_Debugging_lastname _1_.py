ea9954dce1f8492ba7225ce1a4e7d3e09c2e090b
ShadowVexro <79299676+ShadowVexro@users.noreply.github.com>
2021-06-28 20:19:20 -0400

    Add files via upload

---
+++ P3HW1_Debugging_lastname _1_.py
@@ -0,0 +1,28 @@
+# I was supposed to put a comment here
+# My Last Name
+
+def main():
+    # This program takes a number grade and outputs a letter grade.
+
+    # system uses 10-point grading scale
+    A_score = 90
+    # TO DO: define the rest of the scores
+
+    
+score = input('Enter grade: ')
+
+if score >= A_score:
+    print('Your grade is: A')
+elif score > B_score:
+    print('Your grade is: B')
+else:
+    print('Your grade is: F') # TO DO: finish this
+
+
+
+
+
+
+
+# program start
+main()
